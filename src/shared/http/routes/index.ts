3e8131d7c543c3e18c386b516f7cf7113c814678
Gabriel Bernalle <gabrielbbmatozinhos@gmail.com>
2022-02-06 23:17:07 -0300

    Correção de rotas e criação completa rota produtos 'Cardapio'

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,21 +1,12 @@
+import productsRouter from '@modules/products/routes/products.routes';
 import { Router } from 'express';
 
 const routes = Router();
 
+routes.use('/cardapio', productsRouter);
+
 routes.get('/', (request, response) => {
   return response.json({ message: 'Rota Principal! ' });
-});
-
-routes.get('/Cardapio', (request, response) => {
-  return response.json({ message: 'Rota Cardapio! ' });
-});
-
-routes.get('/Pagamento', (request, response) => {
-  return response.json({ message: 'Rota Pagamento! ' });
-});
-
-routes.get('/Edicao', (request, response) => {
-  return response.json({ message: 'Rota Edicao! ' });
 });
 
 export default routes;
